6c45cab1b6e1df0b71cde3df599be7088e191296
Robert Druska <robert.druska@gmail.com>
2025-06-26 18:58:46 +0200

    unofficial reddit api -> PRAW

--- src/reddit.py
+++ src/reddit.py
@@ -1,22 +1,29 @@
-import requests
+
+import os
+import praw
+
+from dotenv import load_dotenv
+load_dotenv()
+
+client_id = os.getenv("REDDIT_CLIENT_ID")
+client_secret = os.getenv("REDDIT_CLIENT_SECRET")
+user_agent = os.getenv("REDDIT_USER_AGENT")
+
+reddit = praw.Reddit(
+	client_id=client_id,
+	client_secret=client_secret,
+	user_agent=user_agent,
+)
 
 def get_posts(subreddit: str, limit: int = 100) -> list[dict]:
-    url = f"https://www.reddit.com/r/{subreddit}.json?limit={limit}"
-    response = requests.get(url, headers={'User-agent': 'genindex'})
-    res = response.json()
-
     posts = []
-    for post in res['data']['children']:
-        if not post['data']['stickied']:
-            title = post['data']['title']
-            content = post['data']['selftext']
-            text = f"{title}\n\n{content}"
+    for submission in reddit.subreddit(subreddit).hot(limit=limit):
+        if not submission.stickied:
             posts.append({
-                'id': post['data']['id'],
-                'url': f"https://www.reddit.com{post['data']['permalink']}",
-                'title': title,
-                'content': content,
-                'text': text,
+                'id': submission.id,
+                'url': submission.url,
+                'title': submission.title,
+                'content': submission.selftext,
+                'text': f"{submission.title}\n\n{submission.selftext}",
             })
-            
     return posts
